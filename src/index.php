891a4f81f4512d14f17ebefc972d97d768e0a315
Nicky Irawan <niwasmala@gmail.com>
2016-02-17 16:14:29 +0700

    Initialize

--- src/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "<h1>Tes</h1>";
-
-return 2;
--- test/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Index extends PHPUnit_Framework_TestCase
-{
-    public function testOnePlusOne() {
-        $this->assertEquals(1+1, include(__DIR__ . '/../src/index.php'));
-    }
-}
